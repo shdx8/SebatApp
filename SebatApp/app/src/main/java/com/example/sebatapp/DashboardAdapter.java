1f2150b847391dcb67f7621d62f8e889b828c7bd
Makhmuda Nurul Khasana <65619513+MakhmudaNurulKhasana@users.noreply.github.com>
2021-06-21 20:25:31 +0700

    First

---
+++ SebatApp/app/src/main/java/com/example/sebatapp/DashboardAdapter.java
@@ -0,0 +1,59 @@
+package com.example.sebatapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.DashboardHolder> {
+    private ArrayList<SetterGetter> listdata;
+
+    public DashboardAdapter(ArrayList<SetterGetter>listdata){
+        this.listdata = listdata;
+    }
+    @NonNull
+    @Override
+    public DashboardHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dashboard,parent, false);
+        DashboardHolder holder = new DashboardHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DashboardHolder holder, int position) {
+        final SetterGetter getData = listdata.get(position);
+        String titlemenu = getData.getTitle();
+        String logomenu = getData.getImg();
+        holder.titleMenu.setText(titlemenu);
+        if(logomenu.equals("logomenu1")) {
+            holder.imgMenu.setImageResource(R.drawable.tambah);
+        } else if (logomenu.equals("logomenu2")) {
+            holder.imgMenu.setImageResource(R.drawable.dipinjam1);
+        } else if (logomenu.equals("logomenu3")) {
+            holder.imgMenu.setImageResource(R.drawable.profit1);
+        } else if (logomenu.equals("logomenu4")) {
+            holder.imgMenu.setImageResource(R.drawable.history);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listdata.size();
+    }
+
+    public class DashboardHolder extends RecyclerView.ViewHolder {
+        TextView titleMenu;
+        ImageView imgMenu;
+        public DashboardHolder(@NonNull View itemView) {
+            super(itemView);
+            titleMenu = itemView.findViewById(R.id.menu_tambah);
+            imgMenu = itemView.findViewById(R.id.logo_menu);
+        }
+    }
+}
---
+++ SebatApp/app/src/main/java/com/example/sebatapp/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.example.sebatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.sebatapp.Util.ServerAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+        // Parameter sesuai dengan tipe data
+        EditText username;
+        EditText password;
+        Button button;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            // Deklarasi komponen view
+            username = (EditText) findViewById(R.id.txt_username);
+            password = (EditText) findViewById(R.id.txt_password);
+            button = (Button) findViewById(R.id.btn_login);
+            // Setting button ketika di klik
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Panggil void sendLogin()
+                    sendLogin();
+                }
+            });
+        }
+        private void sendLogin() {
+            // Setting POST request ke server
+            StringRequest loginRequest = new StringRequest(Request.Method.POST, ServerAPI.URL_LOGIN,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Handle response dari server ketika sukses dengan mengkonvert menjadi JSON
+                            try {
+                                JSONObject json = new JSONObject(response);
+                                // Mengambil variable status pada response
+                                String status = json.getString("status");
+                                if(status.equals("success")){
+                                    // Jika Login Sukses Maka pindah ke activity lain.
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    // Jika Login Gagal Maka mengeluarkan Toast dengan message.
+                                    Toast.makeText(getApplicationContext(), "Username & Password Salah", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Handle response dari server ketika gagal
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put("username", username.getText().toString());
+                    params.put("password", password.getText().toString());
+                    return params;
+                }
+            };
+            // Buat antrian request pada cache android
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            // Tambahkan Request pada antrian request
+            requestQueue.add(loginRequest);
+        }
+    }
