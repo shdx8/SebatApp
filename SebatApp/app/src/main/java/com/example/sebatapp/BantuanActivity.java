f1386d4773f1f3385401d6d8f3a47e32f7df7b08
Makhmuda Nurul Khasana <65619513+MakhmudaNurulKhasana@users.noreply.github.com>
2021-06-22 23:43:54 +0700

    3

--- SebatApp/app/src/main/java/com/example/sebatapp/BantuanActivity.java
+++ SebatApp/app/src/main/java/com/example/sebatapp/BantuanActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DipinjamActivity extends AppCompatActivity {
+public class BantuanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dipinjam);
+        setContentView(R.layout.activity_bantuan);
     }
 }
--- SebatApp/app/src/main/java/com/example/sebatapp/DashboardActivity.java
+++ SebatApp/app/src/main/java/com/example/sebatapp/DashboardActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PenghasilanActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_penghasilan);
+        setContentView(R.layout.activity_dashboard);
     }
 }
